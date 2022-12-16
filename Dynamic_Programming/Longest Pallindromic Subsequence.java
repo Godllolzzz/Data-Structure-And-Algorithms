706002e8e2606d9eacf368fdbe1edaf9bf6a0964
Praveen Chauhan <110886418+Godllolzzz@users.noreply.github.com>
2022-12-16 15:40:05 +0530

    Create Longest Pallindromic Subsequence.java

---
+++ Dynamic_Programming/Longest Pallindromic Subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int m = s.length();
+        StringBuilder sb = new StringBuilder(s);
+        String t = sb.reverse().toString();
+        int n = t.length();
+        int dp[][] = new int[m+1][n+1];
+        for(int i = 0 ; i <= m ; i++){
+            for(int j = 0 ; j <= n ; j++){
+                 if(i == 0 || j == 0) {
+                     dp[i][j] = 0; continue;
+                 }
+                  if(s.charAt(i-1) == t.charAt(j-1))  dp[i][j] = 1 + dp[i-1][j-1];
+                  else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                              }
+        }
+        return dp[m][n];
+    }
+}

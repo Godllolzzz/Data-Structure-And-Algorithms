05615e60cb7aae5b3e6a5ac3a91a4acae993fa04
Praveen Chauhan <110886418+Godllolzzz@users.noreply.github.com>
2022-12-30 21:10:25 +0530

    Create Search in Rotated Sorted Array.java

---
+++ Binary_Search/Search in Rotated Sorted Array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int t) {
+        int low = 0;
+        if(nums[0] == t) return 0;
+        if(nums[nums.length-1] == t) return nums.length-1;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = (high + low + 1)/2;
+            System.out.println(low+" "+mid+" "+high);
+            if(nums[mid] == t) return mid;
+            if(nums[mid]  > nums[low]){
+                if(nums[mid] > t && nums[low] <= t) high = mid - 1;
+                else low = mid + 1;
+            }
+            else{
+                if(nums[mid] < t && nums[high] >= t) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}

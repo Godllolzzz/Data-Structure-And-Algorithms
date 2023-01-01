cbac05759ec783b774703de47c34d77585c8e211
Praveen Chauhan <110886418+Godllolzzz@users.noreply.github.com>
2023-01-01 13:46:06 +0530

    Update Search in Rotated Sorted Array.java

--- Binary_Search/Search in Rotated Sorted Array.java
+++ Binary_Search/Search in Rotated Sorted Array.java
@@ -1,3 +1,5 @@
+// without duplicate 
+
 class Solution {
     public int search(int[] nums, int t) {
         int low = 0;
@@ -18,5 +20,32 @@ class Solution {
             }
         }
         return -1;
+    }
+}
+
+//  with duplicate
+
+class Solution {
+    public boolean search(int[] nums, int t) {
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + (high - low + 1)/2;
+            System.out.println(low+" "+mid+" "+high);
+            if(nums[mid] == t) return true;
+            if(nums[mid] == nums[low] && nums[mid] == nums[high]){
+                low++;
+                high--;
+            }
+            else if(nums[mid]  >= nums[low]){
+                if(nums[mid] > t && nums[low] <= t) high = mid - 1;
+                else low = mid + 1;
+            }
+            else{
+                if(nums[mid] < t && nums[high] >= t) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        return false;
     }
 }
